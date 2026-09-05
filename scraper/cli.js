d9c32fc4be78ed3bf08f751b3e357a81de806e29
Osama4000 <osamaselim878@gmail.com>
2026-09-05 04:54:12 +0300

    Delete scraper directory

--- scraper/cli.js
+++
@@ -1,7 +0,0 @@
-const { runScrape } = require('./lipsScraper');
-runScrape().then(({ metadata }) => {
-  console.log(JSON.stringify(metadata, null, 2));
-}).catch(err => {
-  console.error(err);
-  process.exitCode = 1;
-});
